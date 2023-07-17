49177e4b8bf659ad5ce8735672ed5d6208e43942
Shees_Ali <sheessyed101@gmail.com>
2023-07-17 16:23:31 +0500

    initial commit

---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreApi.Models;
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    public string BookName { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public bool Completed { get; set; } = false;
+    public string Status { get; set; } = null!;
+    public string CompletedOn { get; set; } = null!;
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200", "http://localhost:5288").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+                      });
+});
+
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+builder.Services.AddSingleton<BooksService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.Run();
